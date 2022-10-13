899a5ea7d4ca8b03c9ffc9a4d66a9baf481ec1e0
Timothy Ndichu <44669570+timndichu@users.noreply.github.com>
2022-10-13 19:59:46 +0300

    added receiver and transmitter code

---
+++ Receiver/Receiver-test2/Receiver-test2.ino
@@ -0,0 +1,32 @@
+
+// Code 1 : Sending Text (Receiver)
+// Library: TMRh20/RF24 (https://github.com/tmrh20/RF24/)
+
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+
+RF24 radio(2, 4); // CE, CSN
+const byte address[10] = "ADDRESS01";
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.printDetails();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+};
+
+void loop() {
+  if (radio.available()) {
+    char txt[32] = "";
+    radio.read(&txt, sizeof(txt));
+    Serial.println(txt);
+  }
+//  else {
+//    Serial.println("No radio available");
+//    delay(1000);
+//  }
+};
---
+++ Transmitter/Transmitter-test2/Transmitter-test2.ino
@@ -0,0 +1,30 @@
+
+// Code 1 : Sending Text (Transmitter)
+// Library: TMRh20/RF24 (https://github.com/tmrh20/RF24/)
+
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+
+RF24 radio(4, 5); // CE, CSN
+const byte address[10] = "ADDRESS01";
+
+void setup() {
+  Serial.begin(9600);
+   if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}  // hold in infinite loop
+  }
+  radio.setAutoAck(false);
+  radio.printDetails();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  
+};
+
+void loop() {
+  const char txt[] = "Hello World";
+  radio.write(&txt, sizeof(txt));
+  delay(1000);
+};
